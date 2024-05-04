ddda8a47e120270259ee32ac748ad75df2a3d2a2
iuryss <iuryrbs@edu.unifor.br>
2024-05-04 19:15:42 -0300

    Inicio da leitura de qrcode

---
+++ lib/Telas/home_page_qrcode.dart
@@ -0,0 +1,131 @@
+import 'package:aplicacao_mobile/Telas/settings.dart';
+import 'package:flutter/material.dart';
+import 'package:aplicacao_mobile/components/forum_button.dart';
+import 'package:aplicacao_mobile/Telas/login.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
+void main() => runApp(const HomePageApp());
+
+class HomePageApp extends StatelessWidget {
+  const HomePageApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  int itemSelecionado = 1;
+  String ticket = "";
+   
+  void tapItem(int index){
+    
+    setState((){
+        itemSelecionado = index;
+        if(itemSelecionado == 2){
+          toSettingsPage();
+        }
+    });
+  }
+
+
+  void toLoginPage(){
+    Navigator.push(context, MaterialPageRoute(builder: (context)=>SearchPage()));
+  }
+
+  void toSettingsPage(){
+    Navigator.push(context, MaterialPageRoute(builder: (context) => Settings()));
+  }
+
+  readQrCode() async {
+      String code = await FlutterBarcodeScanner.scanBarcode(
+        "#FFFFFF",
+        "Cancelar",
+        false,
+        ScanMode.QR,
+      );
+      setState(() => ticket = code != "-1" ? code : "Não validado");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(bottom: Radius.circular(20))),
+        toolbarHeight: 130,
+        backgroundColor: Colors.lightBlue,
+        leading: const Icon(Icons.person),
+        title: const SearchBar(
+          keyboardType: TextInputType.text,
+          hintText: "Pesquise aqui",
+          padding: MaterialStatePropertyAll<EdgeInsets>(
+              EdgeInsets.symmetric(horizontal: 16.0)),
+        ),
+        actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.qr_code),
+                onPressed: readQrCode,
+              )
+            ],
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            ForumButton("Forum 1", () {}),
+            const SizedBox(
+              width: 33.0,
+            ),
+            ForumButton("Forum 2", () {}),
+          ]),
+          const SizedBox(
+            height: 50.0,
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            ForumButton("Forum 3", () {}),
+            const SizedBox(
+              width: 33.0,
+            ),
+            ForumButton("Forum 4", () {}),
+          ]),
+          const SizedBox(
+            height: 50.0,
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            ForumButton("Forum 5", () {}),
+            const SizedBox(
+              width: 33.0,
+            ),
+            ForumButton("Forum 6", () {}),
+          ]),
+        ],
+      )),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.lightBlue,
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Perfil',),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home',),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Configurações',),
+        ],
+        selectedItemColor: Colors.black,
+        onTap: tapItem,
+        currentIndex: itemSelecionado,
+
+      ),
+    );
+  }
+}
